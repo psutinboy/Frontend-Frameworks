534ca4d1ff21ad12f0a6269b1e27d62bd0facbb5
Psutinboy <grantledbetter12@gmail.com>
2025-08-27 08:53:56 -0500

    8-27-2025

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,13 +1,16 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import MovieGrid from "./components/MovieGrid";
+import MoviesGrid from "./components/MoviesGrid";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieGrid />
+      <MoviesGrid />
+
+
+
       <Footer />
     </div>
   );
