7a312a5a1956e8241568b75007199c251b175573
Psutinboy <grantledbetter12@gmail.com>
2025-08-29 08:51:56 -0500

    8-29-2025

---
+++ movieapp/src/components/MovieCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles.css";
+
+export default function MovieCard({ movie }) {
+
+  const handleError = (e) => {
+    e.target.src = "images/default.jpg";
+  };
+
+  return (
+      <div className="movie-card" key={movie.id}>
+        <img src={`images/${movie.image}`} alt={movie.title} onError={handleError} />
+        <div className="movie-card-info">
+          <h3 className="movie-card-title">{movie.title}</h3>
+          <p className="movie-card-genre">{movie.genre}</p>
+          <p className="movie-card-rating">{movie.rating}</p>
+        </div>
+      </div>
+  );
+}
