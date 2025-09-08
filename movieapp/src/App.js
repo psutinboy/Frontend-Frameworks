7156e4113b240bea55272473a4a70e224a03e6c8
Psutinboy <grantledbetter12@gmail.com>
2025-09-08 08:49:43 -0500

    9-8-2025

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,16 +1,43 @@
-import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
 import "./styles.css";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
   return (
     <div className="App">
-      <Header />
-      <MoviesGrid />
+      <div className="container">
+        <Header />
 
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
 
-
+          <Routes>
+            <Route path="/" element={<MoviesGrid movies={movies} />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+          </Routes>
+        </Router>
+      </div>
       <Footer />
     </div>
   );
--- movieapp/src/components/MoviesGrid.js
+++ movieapp/src/components/MoviesGrid.js
@@ -1,19 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../styles.css";
 import MovieCard from "./MovieCard";
 
-export default function MoviesGrid() {
-  const [movies, setMovies] = useState([]);
+export default function MoviesGrid({ movies }) {
   const [searchTerm, setSearchTerm] = useState("");
 
   const [genre, setGenre] = useState("All Genres");
   const [rating, setRating] = useState("All Ratings");
-
-  useEffect(() => {
-    fetch("movies.json")
-      .then((response) => response.json())
-      .then((data) => setMovies(data));
-  }, []);
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
