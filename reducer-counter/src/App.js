a3d1be7acc4f396540e66cf07dbc293a7b6544af
Psutinboy <grantledbetter12@gmail.com>
2025-09-15 08:44:45 -0500

    9-15-2025

---
+++ reducer-counter/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Counter App</h1>
+        <Counter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
